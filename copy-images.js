aaf10c09baff0825948f7d1e94463fe668dffa63
Alexey Bashinsky <bashinsky.alexey@gmail.com>
2022-08-29 18:10:03 +0300

    icon

--- copy-images.js
+++ copy-images.js
@@ -23,14 +23,7 @@ const execWithConsole = async (command, removeNewLine) => {
 
 const main = async () => {
   await execWithConsole('pwd', true);
-  await execWithConsole('rm -rf dist');
-  await execWithConsole('mkdir dist');
-  await execWithConsole('cp src/options.html src/popup.html src/options.css src/manifest.json dist');
-  await execWithConsole('npx tsc');
-  await execWithConsole(`NODE_ENV=\'${env}\' npx babel dist --out-dir dist`);
-  if (env === 'production') {
-    await execWithConsole(`cp src/icon128.png src/icon48.png src/icon16.png dist`);
-  }
+  await execWithConsole('cp src/icon128.png src/icon48.png src/icon16.png dist');
 };
 
 main();
